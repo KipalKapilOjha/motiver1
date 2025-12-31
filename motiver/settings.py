264d2d196b38581eb4ec652253798ad97fdfd09d
Kapil Ojha <147418361+KipalKapilOjha@users.noreply.github.com>
2025-12-31 08:55:25 +0545

    Update settings.py

--- motiver/settings.py
+++ motiver/settings.py
@@ -73,16 +73,16 @@ WSGI_APPLICATION = "motiver.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.mysql",
-#         "NAME": "motiver_db",
-#         "USER": "root",
-#         "PASSWORD": "",
-#         "HOST": "localhost",
-#         "PORT": 3306,
-#     }
-# }
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "motiver_db",
+        "USER": "root",
+        "PASSWORD": "",
+        "HOST": "localhost",
+        "PORT": 3306,
+    }
+}
 
 
 # Password validation
